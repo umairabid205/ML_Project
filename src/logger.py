a43ae31fd0beb5e25fd067061ae74ab2a4760910
Umair Abid <umairabid205@gmail.com>
2025-05-21 12:27:09 +0500

    EDA and Problem statement

--- src/logger.py
+++ src/logger.py
@@ -1,21 +1,17 @@
 import logging
 import os
-import sys
 from datetime import datetime
 
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-log_file = f"{datetime.now().strftime('%Y-%m-%d')}.log" # Create a log file with the current date
-log_path = os.path.join(os.getcwd(), "logs", log_file) # Create a path for the log file
-os.makedirs(os.path.dirname(log_path), exist_ok=True) # Create the directory if it doesn't exist
-
-
-log_fild_path = os.path.join(os.getcwd(), "logs", log_file) # Create a path for the log file
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-
-    filename=log_fild_path,
-    format='%(asctime)s %(message)s',
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S'
-) # Configure the logging module to write logs to the log file with a specific format and level
 
+
+)
